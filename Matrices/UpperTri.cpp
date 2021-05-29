154b78d920c7faf9d06e81dc03a6d499dbe49fce
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2021-05-29 23:37:27 +0530

    Added Upper Diagonal prg

--- Matrices/UpperTri.cpp
+++ Matrices/UpperTri.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 using namespace std;
 
-class LowerDiagonal
+class UpperDiagonal
 {
     private:
         int *A;
         int n;
     public:
-        LowerDiagonal(){
+        UpperDiagonal(){
             n=2;
             A=new int[2];
         }
-        LowerDiagonal(int n){
+        UpperDiagonal(int n){
             this->n=n;
             A=new int[(n*(n+1)/2)];
         }
-        ~LowerDiagonal(){
+        ~UpperDiagonal(){
             delete []A;
         }
     void Set(int i,int j,int x);
@@ -26,25 +26,25 @@ class LowerDiagonal
 
 /*Set Method using Row-major formula
     
-    Row major:  Index(A[i][j]) = A[ (i*(i-1)/2) + j-1 ]
-    Column major: Index(A[i][j]) = A[(n*(j-1) - ((j-2)(j-1)/2)) + (i-j)]
+    Row major:  Index(A[i][j]) = A[ (n*(i-1) - ((i-2)*(i-1)/2)) + (j-i) ]
+    Column major: Index(A[i][j]) = A[(j*(j-1)/2) + i-1]
 */
-void LowerDiagonal::Set(int i,int j,int x){
-    if(i>=j)
-        A[(i*(i-1)/2)+j-1]=x;
+void UpperDiagonal::Set(int i,int j,int x){
+    if(i<=j)
+        A[ (n*(i-1) - ((i-2)*(i-1)/2)) + (j-i) ]=x;
 }
 
 
-int LowerDiagonal::Get(int i,int j){
-    if(i>=j)
-    return A[(i*(i-1)/2)+j-1];
+int UpperDiagonal::Get(int i,int j){
+    if(i<=j)
+    return A[ (n*(i-1) - ((i-2)*(i-1)/2)) + (j-i) ];
     return 0;
 }
-void LowerDiagonal::Display(){
+void UpperDiagonal::Display(){
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
-            if(i>=j)
-            cout<<A[(i*(i-1)/2)+j-1]<<" ";
+            if(i<=j)
+            cout<<A[ (n*(i-1) - ((i-2)*(i-1)/2)) + (j-i) ]<<" ";
             else
             cout<<"0 ";
         }
@@ -57,7 +57,7 @@ int main()
     int d;
     cout<<"Enter Dimensions";
     cin>>d;
-    LowerDiagonal dm(d);
+    UpperDiagonal dm(d);
     int x;
     cout<<"Enter All Elements";
     for(int i=1;i<=d;i++){
